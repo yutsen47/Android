c8ca4fb1904e53ac59b3dc2aa40064d6f1d4bf03
etoshi05 <ting94118@gmail.com>
2025-12-27 21:32:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/afinal/WeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.example.afinal
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
