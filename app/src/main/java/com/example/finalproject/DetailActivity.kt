e64a6bb567957ee2fae6dde97e21851894cb5a9f
yutsen <trina940325@gmal.com>
2025-12-28 00:14:13 +0800

    Local changes in DetailActivity.kt

--- app/src/main/java/com/example/finalproject/DetailActivity.kt
+++ app/src/main/java/com/example/finalproject/DetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.R
 
 class DetailActivity : AppCompatActivity() {
 
